937f8144dbb023cc53cba308693280c41560fc8b
Urikke <guilhermenogueira.design@gmail.com>
2021-07-12 20:53:19 -0300

    Alterando o estado dos componentes

--- meu-app/src/index.js
+++ meu-app/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import App2 from './Components/App2';
+import App3 from './Components/App3';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 title="Título">
-      Conteúdo
-    </App2>
-
-    <App2 title="Título2">
-      Conteúdo2
-    </App2>
+    <App3>
+    </App3>
   </React.StrictMode>,
   document.getElementById('root')
 );
